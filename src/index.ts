8356887f9ed9cc5fee6d373e1a211a1c8fdf0ac8
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-07-23 01:48:01 +0300

    octokit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello World!');
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {PushEvent} from '@octokit/webhooks-definitions/schema'
+
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // token: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+
+    if (github.context.eventName !== 'push') {
+      core.info(`Unexpected event: ${github.context.eventName}), exiting`);
+      return;
+    }
+
+    const pushPayload = github.context.payload as PushEvent
+    core.info(`The head commit is: ${pushPayload.head_commit}`)
+
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+    // const { data: pullRequest } = await octokit.rest.pulls.get({
+    //     owner: 'octokit',
+    //     repo: 'rest.js',
+    //     pull_number: 123,
+    //     mediaType: {
+    //       format: 'diff'
+    //     }
+    // });
+
+    // console.log(pullRequest);
+}
+
+run();
