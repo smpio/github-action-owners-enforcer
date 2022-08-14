0f194a56ee0da33179ef597c85e339fd8a2318ab
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-08-14 21:34:35 +0300

    update log

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ async function run() {
       return;
     }
 
-    core.info(`The HEAD commit is: id=${pushPayload.head_commit.id} tree_id=${pushPayload.head_commit.tree_id}`);
+    const ref = pushPayload.ref;
+    const headSha = pushPayload.head_commit.id;
+    const beforeSha = pushPayload.before;
+    const afterSha = pushPayload.after;
+    core.info(`ref=${ref} headSha=${headSha} beforeSha=${beforeSha} afterSha=${afterSha}`);
 
     // This should be a token with access to your repository scoped in as a secret.
     // The YML workflow will need to set myToken with the GitHub Secret Token
@@ -23,20 +27,6 @@ async function run() {
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
-
-    // You can also pass in additional options as a second parameter to getOctokit
-    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-    // const { data: pullRequest } = await octokit.rest.pulls.get({
-    //     owner: 'octokit',
-    //     repo: 'rest.js',
-    //     pull_number: 123,
-    //     mediaType: {
-    //       format: 'diff'
-    //     }
-    // });
-
-    // console.log(pullRequest);
 }
 
 run();
